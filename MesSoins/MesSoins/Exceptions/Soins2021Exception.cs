98a72f708a1a64e582f9d2ab5fe1c2b12722849d
Satmyx <35663463+satmyx@users.noreply.github.com>
2022-09-17 23:00:32 +0200

    Ajout des nouvelles class

---
+++ MesSoins/MesSoins/Exceptions/Soins2021Exception.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Soins2021Exception.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace MesSoins.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Soins2021Exception
+    {
+    }
+}
